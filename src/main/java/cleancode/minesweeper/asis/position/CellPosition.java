81f3efaa75cbbfb8238b957dab98c66e690177e9
JIHO CHOI <wlghsp@gmail.com>
2024-08-16 08:43:07 +0900

    Value Object - 진행 중

---
+++ src/main/java/cleancode/minesweeper/asis/position/CellPosition.java
@@ -0,0 +1,35 @@
+package cleancode.minesweeper.asis.position;
+
+import java.util.Objects;
+
+public class CellPosition {
+
+    private final int rowIndex;
+    private final int colIndex;
+
+    private CellPosition(int rowIndex, int colIndex) {
+        if (rowIndex < 0 || colIndex < 0) {
+            throw new IllegalArgumentException("올바르지 않은 좌표입니다.");
+        }
+
+        this.rowIndex = rowIndex;
+        this.colIndex = colIndex;
+    }
+
+    public static CellPosition of(int rowIndex, int colIndex) {
+        return new CellPosition(rowIndex, colIndex);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CellPosition that = (CellPosition) o;
+        return rowIndex == that.rowIndex && colIndex == that.colIndex;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(rowIndex, colIndex);
+    }
+}
