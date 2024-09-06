2bab20bf2a5a610a2d1e8d281211f51d364600f7
JIHO CHOI <jh.choi2@okestro.com>
2024-09-06 19:02:32 +0900

    [6-4] 리팩토링(3) - FileHandler를 Provider 개념으로 리팩토링
    - 헥사고날 아키텍처 - 포트와 어댑터
    - 포트 - 인터페이스, 어댑터 - 구현체

---
+++ src/main/java/cleancode/studycafe/tobe/io/provider/SeatPassFileReader.java
@@ -0,0 +1,40 @@
+package cleancode.studycafe.tobe.io.provider;
+
+import cleancode.studycafe.tobe.model.pass.StudyCafePassType;
+import cleancode.studycafe.tobe.model.pass.StudyCafeSeatPass;
+import cleancode.studycafe.tobe.model.pass.StudyCafeSeatPasses;
+import cleancode.studycafe.tobe.provider.SeatPassProvider;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SeatPassFileReader implements SeatPassProvider {
+
+
+    private static final String LOCKER_LIST_CSV_PATH = "src/main/resources/cleancode/studycafe/pass-list.csv";
+
+    @Override
+    public StudyCafeSeatPasses getSeatPasses() {
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(LOCKER_LIST_CSV_PATH));
+            List<StudyCafeSeatPass> studyCafePasses = new ArrayList<>();
+            for (String line : lines) {
+                String[] values = line.split(",");
+                StudyCafePassType studyCafePassType = StudyCafePassType.valueOf(values[0]);
+                int duration = Integer.parseInt(values[1]);
+                int price = Integer.parseInt(values[2]);
+                double discountRate = Double.parseDouble(values[3]);
+
+                StudyCafeSeatPass studyCafePass = StudyCafeSeatPass.of(studyCafePassType, duration, price, discountRate);
+                studyCafePasses.add(studyCafePass);
+            }
+
+            return StudyCafeSeatPasses.of(studyCafePasses);
+        } catch (IOException e) {
+            throw new RuntimeException("파일을 읽는데 실패했습니다.", e);
+        }
+    }
+}
