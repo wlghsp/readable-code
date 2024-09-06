686228bdbd0311a04ea6045b2254ecf1836e7bdf
JIHO CHOI <jh.choi2@okestro.com>
2024-09-06 18:16:41 +0900

    [6-3] 리팩토링(2) - PassOrder 개념 추출

--- src/main/java/cleancode/studycafe/tobe/model/pass/StudyCafeSeatPass.java
+++ src/main/java/cleancode/studycafe/tobe/model/pass/StudyCafeSeatPass.java
@@ -48,8 +48,7 @@ public class StudyCafeSeatPass implements StudyCafePass {
         return price;
     }
 
-    public double getDiscountRate() {
-        return discountRate;
+    public int getDiscountPrice() {
+        return (int) (price * discountRate);
     }
-
 }
